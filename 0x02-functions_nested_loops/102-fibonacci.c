02246b3372fbce93bcebad260e47bab3519b5f68
ogbonna chinedu <chinedukingsley54@gmail.com>
2022-10-19 21:59:26 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,17 +10,17 @@ int main(void)
 
 	unsigned long count, i, j, k;
 
-	i = 1;
-	j = 2;
+	i = 0;
+	j = 1;
 
-	for (count = 0; count < 49; count++)
+	for (count = 0; count < 50; count++)
 	{
 		k = i + j;
 		i = j;
 		j = k;
 		printf("%lu", k);
 
-		if (count == 48)
+		if (count == 49)
 			putchar('\n');
 		else
 		{

af9ac2b056527f79af50ddde1a733054e5d860f1
ogbonna chinedu <chinedukingsley54@gmail.com>
2022-10-24 15:54:38 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, followed by a new line.
+ * @s: input string to print
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	
+		while (s[i] != '\0')
+	{
+		_putchar(s[i--]);
+	}
+	_putchar('\n');
+}

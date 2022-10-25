66d71cd4922dc82668209a3e7e34261da8d85c4d
ogbonna chinedu <chinedukingsley54@gmail.com>
2022-10-24 17:35:45 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string pointec to by src bufferpointed to by dest
+ * @src: source to copy
+ * @dest: destination of copy
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}

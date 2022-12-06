ae21e85868e6af8f9b640c4b47381530212c4e88
ogbonna chinedu <chinedukingsley54@gmail.com>
2022-12-05 19:31:49 -0800

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int i = 0, file;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 	if (text_content == NULL)
 		text_content = "";
 

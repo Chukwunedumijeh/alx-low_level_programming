de52aa97caf161949d1fc12c8f3ed254fa11ddc4
ogbonna chinedu <chinedukingsley54@gmail.com>
2023-02-03 17:41:23 -0800

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,19 +12,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht = malloc(sizeof(hash_table_t));
 
 	if (ht == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
 		return (NULL);
-	}
 
 	ht->size = size;
 	ht->array = ht->array = malloc(sizeof(hash_node_t *) * size);
+
 	if (ht->array == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
 		return (NULL);
-	}
+
 	for (; i < size; i++)
 		ht->array[i] = NULL;
+
 	return (ht);
 }
